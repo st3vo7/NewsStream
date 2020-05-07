01124b7c49b1db27da87bfb1fbe9059b0e118e4a
st3vo7 <stefanzm@gmail.com>
2020-05-07 22:17:18 +0200

    pobrisani komentari od prethodnog nacina rada

--- main.py
+++ main.py
@@ -11,50 +11,12 @@ import tornado.web
 import tornado.escape
 
 
-url1 = ( 'https://newsapi.org/v2/top-headlines?'
-        'country=rs&'
-        #'category=technology&'
-        #'q=bomba&'
-        'apiKey=17060bbc869845deb9246555cd6f8e5d')
-
-
-url3 = ( 'https://newsapi.org/v2/sources?'
-        #'language=en&'
-        'country=gb&'
-        #'category=technology&'
-        #'q=manchester city&'
-        'apiKey=17060bbc869845deb9246555cd6f8e5d')
-
-
 from tornado.options import define, options
 define("port",default=8000, help="run on the given port", type=int)
 
 class SourceHandler(tornado.web.RequestHandler):
     def get(self):
 
-        '''
-        print('u getu sam')
-        
-        izvori1 = []
-        
-        #self.render("sources.html")
-        response3 = requests.get(url3)
-        podaci3 = response3.json()
-        
-        status3 = podaci3['status']
-        print(status3)
-        
-        #print(json.dumps(podaci3, indent=1))
-        
-        items3 = podaci3['sources']
-        for index in range(len(items3)):
-            for key in items3[index]:
-                if key == 'name':
-                    izvori1.append(items3[index][key] + ' - ' + items3[index]['description'])
-        
-        '''
-
-        
         self.render("sources.html",
                     )
         
@@ -103,50 +65,10 @@ class SourceHandler(tornado.web.RequestHandler):
             print(podaci3['message'])
 
         self.write(json.dumps({'sent': podaci3}))
-        
-        '''
-        items3 = podaci3['sources']
-        for index in range(len(items3)):
-            for key in items3[index]:
-                if key == 'name':
-                    izvori1.append(items3[index][key] + ' - ' + items3[index]['description'])
-        
 
-        
-        self.render("sources.html",
-                    izvori1=izvori1
-                    )
-        '''
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        
-        naslovi1 = []
-        #naslovi2 = []
-        
-        response1 = requests.get(url1)
-        podaci1 = response1.json()
-        #print(json.dumps(podaci1, indent=1))
-        
-        status1 = podaci1['status']
-        
-        
-        if status1=='error':
-            print(status1)
-            print(podaci1['code'])
-            print(podaci1['message'])
-        
-        
-        print(status1)
-            
-        
-        items1 = podaci1['articles']
-        for index in range(len(items1)):
-            for key in items1[index]:
-                if key == 'title':
-                    naslovi1.append(items1[index][key])
-        
-        
          
         self.render("index.html",
                     )
@@ -170,36 +92,11 @@ class MainHandler(tornado.web.RequestHandler):
         print(dic_data)
         print('---'*26)
 
-        '''
-        upit = self.request.body.decode("utf-8")
-        print('+++'*20)
-        print(upit)
-        print('+++'*20)
-        
-        
-        argumenti = self.request.arguments
-        print(argumenti)
-        print('---'*20)
-        drzava_val = argumenti['country'][0].decode("utf-8") 
-        kategorija_val =  argumenti['kategorija'][0].decode("utf-8")
-
-        primljen = {
-            'country1': drzava_val,
-            'kategorija': kategorija_val
-        }
-        '''
-
 
         a=dic_data['country']
         b=dic_data['category']
         
 
-        '''
-        a=self.get_argument("drzava", None)
-        b=self.get_argument("kategorija",None)
-        print(a)
-        print(b)
-        '''
         
         url = ( 'https://newsapi.org/v2/top-headlines?'
                 'country='+a+'&'
@@ -225,29 +122,8 @@ class MainHandler(tornado.web.RequestHandler):
 
         #print('status:', status1)
         
-        '''
-        items1 = podaci1['articles']
-        for index in range(len(items1)):
-            for key in items1[index]:
-                if key == 'title':
-                    naslovi1.append(items1[index][key])
-
-        #umesto da renderujem celu stranu, vraticu ajaxu podatke, i on ce da mi izgenerise naslove
-        self.write(json.dumps({'status': status1, 'sent': naslovi1}))
-        '''
 
         self.write(json.dumps({'sent': podaci1}))
-
-
-
-        '''
-        #stari nacin, pre ajaksa
-        self.render("index.html",
-                    naslovi1=naslovi1,
-                    zemlja = a,
-                    kat = b,
-                    )
-        '''
 
         print('dosli smo do kraja puta')
         
